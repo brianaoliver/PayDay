7d963b176a4552a737509205b31e4307699c4c0d
Briana Oliver <brianaoli12@gmail.com>
2017-12-26 19:06:36 -0700

    First commit

---
+++ PayDay/PayDay.java
@@ -0,0 +1,11 @@
+/* 
+ this program will determine an individual's paycheck based on hours, pay rate and tax deductions
+ */
+import java.util.*;
+public class PayDay
+{
+    public static void main(String[]args)
+    {
+        
+    }
+}
